bc3beea713e7c4175bc7f079d25ee712e65af00a
Ignacio Arvilla <arvillaignacio@gmail.com>
2024-10-02 11:30:15 +0200

    Creada barra de busqueda temporal y Matches.css

--- client/src/components/Matches.js
+++ client/src/components/Matches.js
@@ -5,6 +5,13 @@ function Partidos() {
     <div className="partidos-container">
       <h1>Partidos de Fútbol</h1>
       <p>Aquí encontrarás la información sobre los partidos más recientes y próximos.</p>
+
+      <div className="search-container">
+        <input type="text" placeHolder="Buscar partido" className="search-input"/>
+        <button className="search-button">
+            <i className="fas fa-search">Buscar</i>
+        </button>
+      </div>
     </div>
   );
 }
