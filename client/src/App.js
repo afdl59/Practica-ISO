e5ab0873add92d84fe976093e642b0fd04b1c9f1
lorensation <lsanztrucharte@gmail.com>
2024-10-02 16:45:48 +0200

    configuracion menu principal añadido seccion Partidos

--- client/src/App.js
+++ client/src/App.js
@@ -16,6 +16,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/partidos" element={<Partidos  />} />
       </Routes>
     </div>
   );
