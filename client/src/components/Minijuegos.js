c218d1dded5a77f315ed7c968764154fca647e1a
Carlos <carlos.richantegutierrez@usp.ceu.es>
2024-10-03 12:57:47 +0200

    bingo2

--- client/src/components/Minijuegos.js
+++ client/src/components/Minijuegos.js
@@ -1,20 +1,27 @@
 // src/components/Minijuegos.js
 import React from 'react';
-import '../styles/Minijuegos.css';
 import { Link } from 'react-router-dom';
+import '../styles/Minijuegos.css'; // Asegúrate de tener estilos para el componente
 
 function Minijuegos() {
   return (
     <div className="minijuegos-container">
-      <h1>Minijuegos de Futbol360</h1>
-      <ul>
-      <li><Link to="/tiro-libre">Tiro Libre</Link></li>
-        <li><a href="/minijuego2">Penaltis</a></li>
-        <li><a href="/minijuego3">Cabeza de Oro</a></li>
-        <li><a href="/minijuego4">Fútbol de Mesa</a></li>
-      </ul>
+      <h1>Minijuegos</h1>
+      <p>¡Elige un minijuego para jugar!</p>
+      <div className="minijuegos-list">
+        <Link to="/minijuegos/bingo" className="minijuego-item">
+          <h2>Bingo de Futbolistas</h2>
+          <p>¡Rellena las casillas con futbolistas conocidos!</p>
+        </Link>
+        <Link to="/tiro-libre" className="minijuego-item">
+          <h2>Tiro Libre</h2>
+          <p>Demuestra tu habilidad con los tiros libres.</p>
+        </Link>
+        {/* Aquí puedes agregar más minijuegos en el futuro */}
+      </div>
     </div>
   );
 }
 
 export default Minijuegos;
+
