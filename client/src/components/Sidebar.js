0b8f1464b81a79924978c4f7694e963e2a4b1a34
lorensation <lsanztrucharte@gmail.com>
2024-10-03 13:18:58 +0200

    actualizado icono del navbar

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -7,15 +7,15 @@ function Sidebar() {
   return (
     <div className="navbar">
       <Link to="/" className="navbar-item">
-        <img src="/../assets/logo.jpeg" alt="Icono de sección" className="navbar-icon" />
+        <img src="../assets/logo.jpeg" alt="Icono de sección" className="navbar-icon" />
         <h3 className="navbar-title">Inicio</h3>
       </Link>
       <Link to="/login" className="navbar-item">
-        <img src="/../assets/logo.jpeg" alt="Icono de sección" className="navbar-icon" />
+        <img src="../assets/logo.jpeg" alt="Icono de sección" className="navbar-icon" />
         <h3 className="navbar-title">Iniciar Sesión</h3>
       </Link>
       <Link to="/register" className="navbar-item">
-        <img src="/../assets/logo.jpeg" alt="Icono de sección" className="navbar-icon" />
+        <img src="../assets/logo.jpeg" alt="Icono de sección" className="navbar-icon" />
         <h3 className="navbar-title">Registrarse</h3>
       </Link>
       <Link to="/partidos" className="navbar-item">
@@ -23,27 +23,27 @@ function Sidebar() {
         <h3 className="navbar-title">Partidos</h3>
       </Link>
       <Link to="/notificaciones" className="navbar-item">
-        <img src="/../assets/logo.jpeg" alt="Icono de sección" className="navbar-icon" />
+        <img src="../assets/logo.jpeg" alt="Icono de sección" className="navbar-icon" />
         <h3 className="navbar-title">Notificaciones personalizadas</h3>
       </Link>
       <Link to="/estadisticas" className="navbar-item">
-        <img src="/../assets/logo.jpeg" alt="Icono de sección" className="navbar-icon" />
+        <img src="../assets/logo.jpeg" alt="Icono de sección" className="navbar-icon" />
         <h3 className="navbar-title">Estadísticas</h3>
       </Link>
       <Link to="/foro" className="navbar-item">
-        <img src="/../assets/logo.jpeg" alt="Icono de sección" className="navbar-icon" />
+        <img src="../assets/logo.jpeg" alt="Icono de sección" className="navbar-icon" />
         <h3 className="navbar-title">Foro</h3>
       </Link>
       <Link to="/minijuegos" className="navbar-item">
-        <img src="/../assets/logo.jpeg" alt="Icono de sección" className="navbar-icon" />
+        <img src="../assets/logo.jpeg" alt="Icono de sección" className="navbar-icon" />
         <h3 className="navbar-title">Minijuegos</h3>
       </Link>
       <Link to="/minijuegos/bingo" className="navbar-item"> {/* Enlace al nuevo juego de Bingo */}
-        <img src="/../assets/logo.jpeg" alt="Icono de sección" className="navbar-icon" />
+        <img src="../assets/logo.jpeg" alt="Icono de sección" className="navbar-icon" />
         <h3 className="navbar-title">Bingo de Futbolistas</h3>
       </Link>
       <Link to="/tiro-libre" className="navbar-item"> {/* Enlace al juego de Tiro Libre */}
-        <img src="/../assets/logo.jpeg" alt="Icono de sección" className="navbar-icon" />
+        <img src="../assets/logo.jpeg" alt="Icono de sección" className="navbar-icon" />
         <h3 className="navbar-title">Tiro Libre</h3>
       </Link>
     </div>
