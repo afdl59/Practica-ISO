574dfc0ed15d331daee61bf33bc56c8c6ea4ce34
raquelcerezocarrion <92672135+raquelcerezocarrion@users.noreply.github.com>
2024-10-04 00:46:47 +0200

    Creacion de Cookies

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,9 +1,23 @@
 // src/components/Home.js
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/Home.css';
 
 function Home() {
+  const [cookiesAccepted, setCookiesAccepted] = useState(false);
+
+  useEffect(() => {
+    const cookiesConsent = localStorage.getItem('cookiesAccepted');
+    if (cookiesConsent) {
+      setCookiesAccepted(true);
+    }
+  }, []);
+
+  const handleAcceptCookies = () => {
+    setCookiesAccepted(true);
+    localStorage.setItem('cookiesAccepted', 'true');
+  };
+
   return (
     <div className="home-container">
       <h1>Bienvenido a Futbol360</h1>
@@ -12,10 +26,17 @@ function Home() {
         <Link to="/login" className="btn">Iniciar Sesión</Link>
         <Link to="/register" className="btn">Registrarse</Link>
       </div>
+
+      {/* Ventana de cookies */}
+      {!cookiesAccepted && (
+        <div className="cookie-banner">
+          <p>Utilizamos cookies para mejorar tu experiencia. Al continuar, aceptas el uso de cookies.</p>
+          <button className="btn-accept-cookies" onClick={handleAcceptCookies}>Aceptar</button>
+        </div>
+      )}
     </div>
   );
 }
 
 export default Home;
-
 
