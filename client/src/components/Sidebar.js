d5b0f3d8a56847cd67237ecb39bc6c86730b154a
raquelcerezocarrion <92672135+raquelcerezocarrion@users.noreply.github.com>
2024-10-03 23:40:36 +0200

    añado minijuegos a Sidebar.js

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -22,13 +22,26 @@ function Sidebar() {
         <img src="/assets/logo.jpg" alt="Icon Partidos" className="navbar-icon" />
         <h3 className="navbar-title">Partidos</h3>
       </Link>
-      {/* Agrega los demás enlaces de la misma manera */}
-      <Link to="/guess-the-player" className="navbar-item">
-        <img src="/assets/logo.jpg" alt="Icon Guess The Player" className="navbar-icon" />
-        <h3 className="navbar-title">Guess the Player</h3>
-      </Link>
+
+      {/* Sección para los minijuegos */}
+      <div className="navbar-item minigames-section">
+        <h3 className="navbar-title">Minijuegos</h3>
+        <Link to="/guess-the-player" className="navbar-item">
+          <img src="/assets/logo.jpg" alt="Icon Guess The Player" className="navbar-icon" />
+          <h3 className="navbar-title">Guess the Player</h3>
+        </Link>
+        <Link to="/tiro-libre" className="navbar-item">
+          <img src="/assets/logo.jpg" alt="Icon Tiro Libre" className="navbar-icon" />
+          <h3 className="navbar-title">Tiro Libre</h3>
+        </Link>
+        <Link to="/bingo" className="navbar-item">
+          <img src="/assets/logo.jpg" alt="Icon Bingo" className="navbar-icon" />
+          <h3 className="navbar-title">Bingo</h3>
+        </Link>
+      </div>
     </div>
   );
 }
 
 export default Sidebar;
+
