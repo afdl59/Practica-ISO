797f57879ce460e3d32beffc3aa16f85f11a8cd1
Carlos <carlos.richantegutierrez@usp.ceu.es>
2024-10-03 12:55:18 +0200

    bingo

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import Home from './components/Home';
 import Login from './components/Login';
 import Register from './components/Register';
 import Minijuegos from './components/Minijuegos';
+import Bingo from './components/Bingo'; // Asegúrate de importar el componente Bingo
 import TiroLibre from './components/TiroLibre';
 import Sidebar from './components/Sidebar';
 import Partidos from './components/Partidos';
@@ -19,12 +20,13 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/minijuegos" element={<Minijuegos />} />
-        <Route path="/tiro-libre" element={<TiroLibre />} />
-        <Route path="/partidos" element={<Partidos  />} />
-
+        <Route path="/minijuegos/bingo" element={<Bingo />} /> {/* Ruta para el Bingo */}
+        <Route path="/tiro-libre" element={<TiroLibre />} /> {/* Ruta para el Tiro Libre */}
+        <Route path="/partidos" element={<Partidos />} />
       </Routes>
     </div>
   );
 }
 
 export default App;
+
--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -6,8 +6,20 @@ import '../styles/Sidebar.css';
 function Sidebar() {
   return (
     <div className="navbar">
-      <Link to="/partidos" className="navbar_item">
-        <img src="/../assets/logo.jpeg" alt="Icono de seccion" className="navbar-icon" />
+      <Link to="/" className="navbar-item">
+        <img src="/../assets/logo.jpeg" alt="Icono de sección" className="navbar-icon" />
+        <h3 className="navbar-title">Inicio</h3>
+      </Link>
+      <Link to="/login" className="navbar-item">
+        <img src="/../assets/logo.jpeg" alt="Icono de sección" className="navbar-icon" />
+        <h3 className="navbar-title">Iniciar Sesión</h3>
+      </Link>
+      <Link to="/register" className="navbar-item">
+        <img src="/../assets/logo.jpeg" alt="Icono de sección" className="navbar-icon" />
+        <h3 className="navbar-title">Registrarse</h3>
+      </Link>
+      <Link to="/partidos" className="navbar-item">
+        <img src="/../assets/logo.jpeg" alt="Icono de sección" className="navbar-icon" />
         <h3 className="navbar-title">Partidos</h3>
       </Link>
       <Link to="/notificaciones" className="navbar-item">
@@ -26,8 +38,17 @@ function Sidebar() {
         <img src="/../assets/logo.jpeg" alt="Icono de sección" className="navbar-icon" />
         <h3 className="navbar-title">Minijuegos</h3>
       </Link>
+      <Link to="/minijuegos/bingo" className="navbar-item"> {/* Enlace al nuevo juego de Bingo */}
+        <img src="/../assets/logo.jpeg" alt="Icono de sección" className="navbar-icon" />
+        <h3 className="navbar-title">Bingo de Futbolistas</h3>
+      </Link>
+      <Link to="/tiro-libre" className="navbar-item"> {/* Enlace al juego de Tiro Libre */}
+        <img src="/../assets/logo.jpeg" alt="Icono de sección" className="navbar-icon" />
+        <h3 className="navbar-title">Tiro Libre</h3>
+      </Link>
     </div>
   );
 }
 
 export default Sidebar;
+
