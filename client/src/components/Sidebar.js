2527c8a06a02f0b7e748aef0ca5f4b43d473f011
raquelcerezocarrion <raquel.cerezocarrion>
2024-10-06 22:54:59 +0200

    actualizo js de sidebar

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -12,44 +12,47 @@ function Sidebar() {
   };
 
   return (
-    <div className="navbar">
-      <Link to="/" className="navbar-item">
-        <img src={logo} alt="Icon Inicio" className="navbar-icon" />
-        <h3 className="navbar-title">Inicio</h3>
-      </Link>
-      <Link to="/login" className="navbar-item">
-        <img src={logo} alt="Icon Iniciar Sesión" className="navbar-icon" />
-        <h3 className="navbar-title">Iniciar Sesión</h3>
-      </Link>
-      <Link to="/register" className="navbar-item">
-        <img src={logo} alt="Icon Registrarse" className="navbar-icon" />
-        <h3 className="navbar-title">Registrarse</h3>
-      </Link>
-      <Link to="/partidos" className="navbar-item">
-        <img src={logo} alt="Icon Partidos" className="navbar-icon" />
-        <h3 className="navbar-title">Partidos</h3>
-      </Link>
-      {/* Minijuegos - Menú desplegable */}
-      <div className="navbar-item dropdown" onClick={toggleMinigames}>
-        <img src={logo} alt="Icon Minijuegos" className="navbar-icon" />
-        <h3 className="navbar-title">Minijuegos</h3>
-        {minigamesOpen && (
-          <div className="dropdown-content">
-            <Link to="/minijuegos/guess-the-player" className="dropdown-item">
-              Guess the Player
-            </Link>
-            <Link to="/minijuegos/tiro-libre" className="dropdown-item">
-              Tiro Libre
-            </Link>
-            <Link to="/minijuegos/bingo" className="dropdown-item">
-              Bingo
-            </Link>
-          </div>
-        )}
+    <div className="navbar-container">
+      <div className="navbar">
+        <Link to="/" className="navbar-item">
+          <img src={logo} alt="Icon Inicio" className="navbar-icon" />
+          <h3 className="navbar-title">Inicio</h3>
+        </Link>
+        <Link to="/login" className="navbar-item">
+          <img src={logo} alt="Icon Iniciar Sesión" className="navbar-icon" />
+          <h3 className="navbar-title">Iniciar Sesión</h3>
+        </Link>
+        <Link to="/register" className="navbar-item">
+          <img src={logo} alt="Icon Registrarse" className="navbar-icon" />
+          <h3 className="navbar-title">Registrarse</h3>
+        </Link>
+        <Link to="/partidos" className="navbar-item">
+          <img src={logo} alt="Icon Partidos" className="navbar-icon" />
+          <h3 className="navbar-title">Partidos</h3>
+        </Link>
+        {/* Minijuegos - Menú desplegable */}
+        <div className="navbar-item dropdown" onClick={toggleMinigames}>
+          <img src={logo} alt="Icon Minijuegos" className="navbar-icon" />
+          <h3 className="navbar-title">Minijuegos</h3>
+          {minigamesOpen && (
+            <div className="dropdown-content">
+              <Link to="/minijuegos/guess-the-player" className="dropdown-item">
+                Guess the Player
+              </Link>
+              <Link to="/minijuegos/tiro-libre" className="dropdown-item">
+                Tiro Libre
+              </Link>
+              <Link to="/minijuegos/bingo" className="dropdown-item">
+                Bingo
+              </Link>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
 }
 
 export default Sidebar;
+
 
