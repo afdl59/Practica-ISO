dd599e06d619a103a5766a00f1b40c7b2e1ad494
richante <121907146+Richante@users.noreply.github.com>
2024-10-14 13:24:28 +0200

    Update TiroLibre.js

--- client/src/components/minijuegos/TiroLibre.js
+++ client/src/components/minijuegos/TiroLibre.js
@@ -1,12 +1,20 @@
 import React, { useState, useEffect } from 'react';
 import '../../styles/TiroLibre.css';
 
+// Importar las imágenes directamente
+import falcao2012 from '../assets/players/falcao2012.jpg';
+import cristiano2012 from '../assets/players/cristiano2012.jpg';
+import messi2012 from '../assets/players/messi2012.jpg';
+import griezmann2016 from '../assets/players/griezmann2016.jpg';
+import ferran2022 from '../assets/players/ferran2022.jpg';
+
+// Mapeo de jugadores con probabilidades y rutas de imágenes
 const players = {
-  falcao: { leftNear: 0.9, leftFar: 0.7, rightNear: 0.6, rightFar: 0.4, image: 'falcao2012.jpg' },
-  cristiano: { leftNear: 0.95, leftFar: 0.95, rightNear: 0.95, rightFar: 0.95, image: 'cristiano2012.jpg' },
-  messi: { leftNear: 0.9, leftFar: 0.85, rightNear: 0.9, rightFar: 0.85, image: 'messi2012.jpg' },
-  griezmann: { leftNear: 0.6, leftFar: 0.8, rightNear: 0.8, rightFar: 0.9, image: 'griezmann2016.jpg' },
-  ferran: { leftNear: 0.4, leftFar: 0.3, rightNear: 0.3, rightFar: 0.2, image: 'ferran2022.jpg' },
+  falcao: { leftNear: 0.9, leftFar: 0.7, rightNear: 0.6, rightFar: 0.4, image: falcao2012 },
+  cristiano: { leftNear: 0.95, leftFar: 0.95, rightNear: 0.95, rightFar: 0.95, image: cristiano2012 },
+  messi: { leftNear: 0.9, leftFar: 0.85, rightNear: 0.9, rightFar: 0.85, image: messi2012 },
+  griezmann: { leftNear: 0.6, leftFar: 0.8, rightNear: 0.8, rightFar: 0.9, image: griezmann2016 },
+  ferran: { leftNear: 0.4, leftFar: 0.3, rightNear: 0.3, rightFar: 0.2, image: ferran2022 },
 };
 
 const sides = ['left', 'right'];
@@ -34,7 +42,7 @@ function TiroLibre() {
       <div className="players">
         {Object.keys(players).map((player) => (
           <button key={player} onClick={() => setSelectedPlayer(player)}>
-            <img src={require(`../assets/${players[player].image}`)} alt={player} />
+            <img src={players[player].image} alt={player} />
           </button>
         ))}
       </div>
@@ -45,6 +53,5 @@ function TiroLibre() {
 }
 
 export default TiroLibre;
-
 
 
