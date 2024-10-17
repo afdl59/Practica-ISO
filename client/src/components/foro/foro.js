5c1834a6e365e97b1293de6895e3131b2a7c8935
Raimundo-baca <124136616+Raimundo-baca@users.noreply.github.com>
2024-10-17 12:31:31 +0200

    Update foro.js

--- client/src/components/foro/foro.js
+++ client/src/components/foro/foro.js
@@ -2,6 +2,8 @@
 import React, { useState, useEffect } from 'react';
 import '../../styles/foro/foro.css';
 
+import io from 'socket.io-client';
+
 
 // Importar el cliente de Socket.IO
 const socket = io();
