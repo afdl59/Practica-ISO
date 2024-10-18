15cf6970b07067b8a5311b3ee4c7ed3fd90d5d6d
Ignacio Arvilla <arvillaignacio@gmail.com>
2024-10-18 10:26:06 +0200

    solucionado imports css

--- client/src/components/stats/Partidos.js
+++ client/src/components/stats/Partidos.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import '../styles/stats/Partidos.css';
+import '../../styles/stats/Partidos.css';
 
 function Partidos() {
   const [partidos, setPartidos] = useState([]);
