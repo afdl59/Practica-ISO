0b3eee8a3010f5493b5862748f11bacb39b1c754
lorensation <lsanztrucharte@gmail.com>
2024-10-18 11:30:25 +0200

    primer commit rama minijuegos

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const mysql = require('mysql2');
 const http = require('http');
 const { Server } = require('socket.io');
 
-
+//prueba rama minijuegos
 const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
