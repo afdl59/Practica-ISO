c73dce9475e656ead692274f00784c878a4db884
lorensation <lsanztrucharte@gmail.com>
2024-10-18 11:28:56 +0200

    primer commit rama perfil

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const mysql = require('mysql2');
 const http = require('http');
 const { Server } = require('socket.io');
 
-
+//prueba rama perfil
 const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
