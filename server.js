eaccc54c6496119b338507cb119f850a3b53a292
lorensation <lsanztrucharte@gmail.com>
2024-10-25 10:10:43 +0200

    configurado CORS en el backend para permitir solicitudes HTTPS

--- server.js
+++ server.js
@@ -168,6 +168,14 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
 });
 
+// Middleware para permitir CORS
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
 // Ruta para manejar el registro
 app.post('/api/register', async (req, res) => {
     const { username, firstName, lastName, email, password } = req.body;
