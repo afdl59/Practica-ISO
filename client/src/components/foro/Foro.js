e142a8e75ccd00f4e620a887d41730f52f5915aa
lorensation <lsanztrucharte@gmail.com>
2024-10-28 09:39:03 +0100

    añadida funcionalidad de crear salas de chat

--- client/src/components/foro/Foro.js
+++ client/src/components/foro/Foro.js
@@ -2,36 +2,42 @@ import React, { useState, useEffect } from 'react';
 import '../../styles/foro/Foro.css';
 import io from 'socket.io-client';
 
-let socket;
-
 function Foro() {
-  const [salas, setSalas] = useState([]);
   const [mensajes, setMensajes] = useState([]);
-  const [currentSala, setCurrentSala] = useState(null);
   const [content, setContent] = useState('');
   const [username, setUsername] = useState('');
+  const [salas, setSalas] = useState([]); // Almacena las salas disponibles
+  const [currentSala, setCurrentSala] = useState(''); // Almacena la sala seleccionada
+  const [newSala, setNewSala] = useState(''); // Almacena el nombre de la nueva sala
+
+  // Inicializar la conexión de Socket.IO
+  const socket = io('https://futbol360.ddns.net');
 
   useEffect(() => {
-    // Obtener el usuario del localStorage
-    const savedUsername = localStorage.getItem('username');
-    if (savedUsername) {
-      setUsername(savedUsername);
-    }
-
-    socket = io('https://futbol360.ddns.net');
-
-    // Cargar las salas de chat iniciales
-    const cargarSalas = async () => {
+    const checkAuthAndLoadData = async () => {
       try {
-        const response = await fetch('/api/foro/salas');
+        // Verificar si el usuario está autenticado
+        const response = await fetch('/api/check-session');
         const data = await response.json();
-        setSalas(data);
+
+        if (!data.isAuthenticated) {
+          navigate('/login'); // Redirige al login si no está autenticado
+          return;
+        }
+
+        setUsername(data.username);
+
+        // Cargar las salas de chat iniciales
+        const responseSalas = await fetch('/api/foro/salas');
+        const dataSalas = await responseSalas.json();
+        setSalas(dataSalas);
       } catch (error) {
-        console.error('Error cargando las salas:', error);
+        console.error('Error verificando la sesión o cargando las salas:', error);
+        navigate('/login');
       }
     };
 
-    cargarSalas();
+    checkAuthAndLoadData();
 
     // Configurar Socket.IO para escuchar los mensajes nuevos
     socket.on('mensajeRecibido', (mensaje) => {
@@ -47,26 +53,38 @@ function Foro() {
     };
   }, [currentSala]);
 
-  const handleSalaSelect = async (salaId) => {
-    setCurrentSala(salaId);
+  const handleSalaChange = (sala) => {
+    setCurrentSala(sala);
+    setMensajes([]); // Limpiar mensajes cuando se cambia de sala
+  };
 
-    // Unirse a la sala con Socket.IO
-    socket.emit('unirseASala', salaId);
+  const handleCreateSala = async (e) => {
+    e.preventDefault();
+    if (newSala) {
+      try {
+        const response = await fetch('/api/foro/salas', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ nombre: newSala }),
+        });
 
-    // Cargar mensajes de la sala seleccionada
-    try {
-      const response = await fetch(`/api/foro/salas/${salaId}/mensajes`);
-      const data = await response.json();
-      setMensajes(data);
-    } catch (error) {
-      console.error('Error cargando los mensajes:', error);
+        if (response.ok) {
+          const nuevaSala = await response.json();
+          setSalas((prevSalas) => [...prevSalas, nuevaSala]);
+          setNewSala(''); // Limpiar el campo de texto
+        }
+      } catch (error) {
+        console.error('Error creando la nueva sala:', error);
+      }
     }
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
     if (username && content && currentSala) {
-      const nuevoMensaje = { salaId: currentSala, userId: username, content, date: new Date() };
+      const nuevoMensaje = { username, content, chatRoom: currentSala, date: new Date() };
       // Emitir el evento de nuevo mensaje al servidor
       socket.emit('nuevoMensaje', nuevoMensaje);
       setContent(''); // Limpiar el campo de texto
@@ -74,53 +92,58 @@ function Foro() {
   };
 
   return (
-    <div className="foro">
+    <>
       <h1>Foro</h1>
-
-      <div className="salas-lista">
-        <h2>Salas de Chat</h2>
+      <div className="salas">
+        <h2>Salas de chat</h2>
         {salas.length === 0 ? (
-          <p>No hay salas disponibles.</p>
+          <p>No hay salas disponibles. ¡Crea una nueva!</p>
         ) : (
-          salas.map((sala) => (
-            <div key={sala._id} onClick={() => handleSalaSelect(sala._id)} className="sala">
-              <h3>{sala.title}</h3>
-              <p>{sala.description}</p>
-            </div>
+          salas.map((sala, index) => (
+            <button key={index} onClick={() => handleSalaChange(sala.nombre)}>
+              {sala.nombre}
+            </button>
           ))
         )}
+        <form onSubmit={handleCreateSala}>
+          <input
+            type="text"
+            placeholder="Nueva sala"
+            value={newSala}
+            onChange={(e) => setNewSala(e.target.value)}
+            required
+          />
+          <button type="submit">Crear Sala</button>
+        </form>
       </div>
-
-      {currentSala && (
-        <>
-          <div id="foro">
-            {mensajes.length === 0 ? (
-              <p>No hay mensajes en esta sala aún. ¡Sé el primero en enviar uno!</p>
-            ) : (
-              mensajes.map((mensaje, index) => (
-                <div key={index}>
-                  <strong>{mensaje.user.username}</strong>: {mensaje.content}
-                  <small style={{ float: 'right' }}>{new Date(mensaje.date).toLocaleString()}</small>
-                  <hr />
-                </div>
-              ))
-            )}
-          </div>
-
-          <form id="formMensaje" onSubmit={handleSubmit}>
-            <input
-              id="content"
-              type="text"
-              placeholder="Mensaje"
-              value={content}
-              onChange={(e) => setContent(e.target.value)}
-              required
-            />
-            <button type="submit">Enviar</button>
-          </form>
-        </>
-      )}
-    </div>
+      <div className="foro">
+        <h2>Sala: {currentSala}</h2>
+        <div id="foro">
+          {mensajes.length === 0 ? (
+            <p>No hay mensajes aún. ¡Sé el primero en enviar uno!</p>
+          ) : (
+            mensajes.map((mensaje, index) => (
+              <div key={index}>
+                <strong>{mensaje.username}</strong>: {mensaje.content}
+                <small style={{ float: 'right' }}>{new Date(mensaje.date).toLocaleString()}</small>
+                <hr />
+              </div>
+            ))
+          )}
+        </div>
+        <form id="formMensaje" onSubmit={handleSubmit}>
+          <input
+            id="content"
+            type="text"
+            placeholder="Mensaje"
+            value={content}
+            onChange={(e) => setContent(e.target.value)}
+            required
+          />
+          <button type="submit">Enviar</button>
+        </form>
+      </div>
+    </>
   );
 }
 
