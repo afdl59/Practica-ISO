d3c9c731bf656cfb1108248a45b4dc092d5367a5
lorensation <lsanztrucharte@gmail.com>
2024-11-06 12:55:43 +0100

    error fix del export connectdb

--- server/config/db.js
+++ server/config/db.js
@@ -1,8 +1,10 @@
 // config/db.js
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI)
+const connectDB = async () => {
+    mongoose.connect(process.env.MONGODB_URI)
     .then(() => console.log('Conectado a MongoDB'))
     .catch(err => console.error('Error de conexión con MongoDB:', err));
+};
 
 module.exports = connectDB;
