6df732fe7a85014aa0da9c03b45ace3b3d4f8d5d
Carlos <carlos.richantegutierrez@usp.ceu.es>
2024-11-08 10:55:33 +0100

    prueba de funcionamiento

--- client/src/components/minijuegos/WordleDiario.js
+++ client/src/components/minijuegos/WordleDiario.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import '../../styles/minijuegos/WordleDiario.css';  // Importa la hoja de estilos
+import '../../styles/minijuegos/WordleDiario.css';  // Importa la hoja de estilos css
 
 const jugadores = [
   'Pele', 'Maradona', 'Zidane', 'Ronaldo', 'Ronaldinho', 'Messi', 'Cristiano Ronaldo', 'Johan Cruyff', 
