50ac828c3957be9845d0081bb4b66548764e13ac
Ignacio Arvilla <arvillaignacio@gmail.com>
2024-11-10 17:27:12 +0100

    Creadas las direcciones para añadir a favoritos en perfil

--- client/src/components/user/Perfil.js
+++ client/src/components/user/Perfil.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
 import '../../styles/user/Perfil.css';
 
 function Perfil() {
@@ -9,7 +9,10 @@ function Perfil() {
   const [editedData, setEditedData] = useState({
     firstName: '',
     lastName: '',
-    equipoFavorito: '',
+    equiposFavoritos: [],
+    competicionesFavoritas: [],
+    equipoFavoritoManual: '', 
+    competicionFavoritaManual: '', 
     fotoPerfil: null
   });
 
@@ -18,27 +21,25 @@ function Perfil() {
       try {
         const response = await fetch('/api/auth/check-session');
         const data = await response.json();
-  
         if (!data.isAuthenticated) {
           navigate('/login');
           return;
         }
-  
         const userResponse = await fetch(`/api/users/${data.username}`);
-        if (!userResponse.ok) {
+
+        if (!userResponse.ok){
           throw new Error('Error al obtener datos del usuario');
         }
-  
+
         const userData = await userResponse.json();
         setUserData(userData);
-  
         setEditedData({
-          username: userData.username, 
+          ...editedData,
           firstName: userData.firstName,
           lastName: userData.lastName,
-          equipoFavorito: userData.equipoFavorito,
-          intereses: userData.intereses,
-          fotoPerfil: userData.fotoPerfil,
+          equiposFavoritos: userData.equiposFavoritos || [],
+          competicionesFavoritas: userData.competicionesFavoritas || [],
+          fotoPerfil: userData.fotoPerfil
         });
       } catch (error) {
         console.error('Error:', error);
@@ -47,22 +48,42 @@ function Perfil() {
         setLoading(false);
       }
     };
-  
     checkAuth();
   }, [navigate]);
-  
 
   const handleLogout = async () => {
     try {
-      const response = await fetch('/api/users/logout', {
-        method: 'POST'
-      });
-
-      if (response.ok) {
+      const response = await fetch('/api/users/logout', { method: 'POST' });
+      if (response.ok){
         navigate('/login');
       }
     } catch (error) {
       console.error('Error al cerrar sesión:', error);
+    }
+  };
+
+  const handleFotoChange = async (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      const formData = new FormData();
+      formData.append('fotoPerfil', file);
+
+      try {
+        const response = await fetch('/api/users/upload', { method: 'POST', body: formData });
+        
+        if (!response.ok){
+          throw new Error('Error al subir la imagen');
+        }
+
+        const data = await response.json();
+        setUserData((prevUserData) => ({
+          ...prevUserData,
+          fotoPerfil: data.imageUrl
+        }));
+        alert('Imagen subida correctamente');
+      } catch (error) {
+        console.error('Error al subir la imagen:', error);
+      }
     }
   };
 
@@ -71,73 +92,40 @@ function Perfil() {
     setEditedData({ ...editedData, [name]: value });
   };
 
-  const handleFotoChange = async (e) => {
-    const file = e.target.files[0];
-    if (file) {
-      const formData = new FormData();
-      formData.append('fotoPerfil', file);
-      formData.append('username', userData.username); // Asegúrate de enviar el nombre de usuario
-  
-      try {
-        const response = await fetch('/api/users/upload', {
-          method: 'POST',
-          body: formData
-        });
-  
-        if (!response.ok) {
-          throw new Error('Error al subir la imagen');
-        }
-  
-        const data = await response.json();
-        setUserData((prevUserData) => ({
-          ...prevUserData,
-          fotoPerfil: data.imageUrl
-        }));
-  
-        alert('Imagen subida correctamente');
-      } catch (error) {
-        console.error('Error al subir la imagen:', error);
-      }
-    }
-  };
-  
-
   const handleSaveChanges = async () => {
     try {
       const updatedData = {
         firstName: editedData.firstName,
         lastName: editedData.lastName,
-        equipoFavorito: editedData.equipoFavorito,
-        intereses: editedData.intereses,
+        equiposFavoritos: editedData.equiposFavoritos.includes(editedData.equipoFavoritoManual)
+          ? editedData.equiposFavoritos
+          : [...editedData.equiposFavoritos, editedData.equipoFavoritoManual],
+        competicionesFavoritas: editedData.competicionesFavoritas.includes(editedData.competicionFavoritaManual)
+          ? editedData.competicionesFavoritas
+          : [...editedData.competicionesFavoritas, editedData.competicionFavoritaManual]
       };
-      console.log('userData antes del fetch:', editedData);
-      console.log('updatedData:', updatedData);
-      const response = await fetch(`/api/users/${editedData.username}`, {
+      const response = await fetch(`/api/users/${userData.username}`, {
         method: 'PUT',
-        headers: {
-          'Content-Type': 'application/json'
-        },
+        headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(updatedData)
       });
-  
-      if (!response.ok) {
+
+      if (!response.ok){
         throw new Error('Error al actualizar los datos del usuario');
       }
-  
-      const updatedUserData = await response.json();
-      setUserData(updatedUserData);
+
+      setUserData(updatedData);
       alert('Datos actualizados correctamente');
     } catch (error) {
       console.error('Error al actualizar los datos del usuario:', error);
     }
   };
-  
 
-  if (loading) {
+  if (loading){
     return <div>Cargando...</div>;
   }
 
-  if (!userData) {
+  if (!userData){
     return <div>Error al cargar los datos del usuario.</div>;
   }
 
@@ -154,39 +142,28 @@ function Perfil() {
       </div>
 
       <div className="perfil-info">
-        <label>
-          Nombre:
-          <input
-            type="text"
-            name="firstName"
-            value={editedData.firstName}
-            onChange={handleInputChange}
-          />
+        <label>Nombre:
+          <input type="text" name="firstName" value={editedData.firstName} onChange={handleInputChange} />
         </label>
-
-        <label>
-          Apellido:
-          <input
-            type="text"
-            name="lastName"
-            value={editedData.lastName}
-            onChange={handleInputChange}
-          />
-        </label>
-
-        <label>
-          Equipo Favorito:
-          <input
-            type="text"
-            name="equipoFavorito"
-            value={editedData.equipoFavorito}
-            onChange={handleInputChange}
-          />
+        <label>Apellido:
+          <input type="text" name="lastName" value={editedData.lastName} onChange={handleInputChange} />
         </label>
       </div>
 
-      <div className="ultimo-login">
-        <p>Último inicio de sesión: {userData.ultimoLogin || 'Nunca'}</p>
+      <div className="favoritos">
+        <h3>Equipos Favoritos</h3>
+        <ul>
+          {editedData.equiposFavoritos.map((equipo, index) => <li key={index}>{equipo}</li>)}
+        </ul>
+        <input type="text" name="equipoFavoritoManual" value={editedData.equipoFavoritoManual} onChange={handleInputChange} />
+        <Link to="/perfil/anadir-equipo-favorito"><button>Añadir equipo favorito</button></Link>
+
+        <h3>Competiciones Favoritas</h3>
+        <ul>
+          {editedData.competicionesFavoritas.map((competicion, index) => <li key={index}>{competicion}</li>)}
+        </ul>
+        <input type="text" name="competicionFavoritaManual" value={editedData.competicionFavoritaManual} onChange={handleInputChange} />
+        <Link to="/perfil/anadir-competicion-favorita"><button>Añadir competición favorita</button></Link>
       </div>
 
       <button onClick={handleSaveChanges}>Guardar Cambios</button>
