21c21c3fc929add520f38d11918faf19db4ca052
Ignacio Arvilla <arvillaignacio@gmail.com>
2024-11-24 15:20:14 +0100

    Creada seccion de Predicciones

---
+++ client/src/components/stats/Predicciones.js
@@ -0,0 +1,12 @@
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+function Predicciones(){
+    return (
+        <div>
+            <h1>Proximamente...</h1>
+        </div>
+    );
+}
+
+export default Predicciones;
