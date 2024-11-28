0f5a728b81cbd65abf8089b22bf601f38545583f
lorensation <lsanztrucharte@gmail.com>
2024-11-28 19:57:56 +0100

    probando otra configuracion

--- client/jest.config.js
+++ client/jest.config.js
@@ -1,10 +1,14 @@
 module.exports = {
     testEnvironment: 'jsdom',
+    transform: {
+        '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest' // Transforma archivos JS/TS usando Babel
+    },
     transformIgnorePatterns: [
-        '/node_modules/(?!@bundled-es-modules|msw)' // Transforma módulos ESM específicos
+        '/node_modules/(?!msw)' // Transforma específicamente el módulo `msw`
     ],
     moduleNameMapper: {
-        '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
-        '\\.(jpg|jpeg|png|gif|webp|svg)$': '<rootDir>/__mocks__/fileMock.js'
+        '\\.(css|less|scss|sass)$': 'identity-obj-proxy', // Mock para estilos
+        '\\.(jpg|jpeg|png|gif|webp|svg)$': '<rootDir>/__mocks__/fileMock.js' // Mock para archivos multimedia
     }
 };
+
