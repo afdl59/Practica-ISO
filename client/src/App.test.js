f9837b0e7ac9a48b04f456de1f142e972799c3b9
lorensation <lsanztrucharte@gmail.com>
2024-11-28 19:48:17 +0100

    envolviendo el app.test en el memory router

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,16 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom'; // Importa MemoryRouter
 import App from './App';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+    render(
+        <MemoryRouter> {/* Envuelve App en un MemoryRouter */}
+            <App />
+        </MemoryRouter>
+    );
+    
+    const linkElement = screen.getByText(/learn react/i); // Ajusta esto según el texto esperado
+    expect(linkElement).toBeInTheDocument();
 });
+
