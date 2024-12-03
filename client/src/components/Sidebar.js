b0fb8ff23708ea16ccca40ab229c849b10e69edc
lorensation <lsanztrucharte@gmail.com>
2024-12-03 19:53:36 +0100

    actualizada Sidebar.js

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -4,63 +4,71 @@ import { FaHome, FaUser, FaComments, FaChartBar, FaGamepad, FaBell } from 'react
 import '../styles/Sidebar.css';
 
 function Sidebar() {
-  const [notificaciones, setNotificaciones] = useState([]);
+  const [minigamesOpen, setMinigamesOpen] = useState(false);
+  const [estadisticasOpen, setEstadisticasOpen] = useState(false);
 
-  useEffect(() => {
-    const savedEquipo = localStorage.getItem('equipoFavorito');
-    const savedIntereses = JSON.parse(localStorage.getItem('intereses')) || [];
+  const toggleMinigames = () => {
+    setMinigamesOpen(!minigamesOpen);
+  };
 
-    const nuevasNotificaciones = [];
-    if (savedEquipo) {
-      nuevasNotificaciones.push(`Noticias recientes de ${savedEquipo}`);
-    }
-
-    savedIntereses.forEach(interes => {
-      nuevasNotificaciones.push(`Actualización en ${interes}`);
-    });
-
-    setNotificaciones(nuevasNotificaciones);
-  }, []);
+  const toggleEstadisticas = () => {
+    setEstadisticasOpen(!estadisticasOpen);
+  };
 
   return (
-    <div className="navbar-horizontal">
+    <div className="navbar">
       <Link to="/" className="navbar-item">
-        <FaHome className="navbar-icon" />
-        <span className="navbar-title">Inicio</span>
+        <img src={FaHome} alt="Icon Inicio" className="navbar-icon" />
+        <h3 className="navbar-title">Inicio</h3>
       </Link>
       <Link to="/perfil" className="navbar-item">
-        <FaUser className="navbar-icon" />
-        <span className="navbar-title">Perfil</span>
+        <img src={FaUser} alt="Icon Perfil" className="navbar-icon" />
+        <h3 className="navbar-title">Perfil</h3>
       </Link>
       <Link to="/foro" className="navbar-item">
-        <FaComments className="navbar-icon" />
-        <span className="navbar-title">Foro</span>
+        <img src={FaComments} alt="Icon Foro" className="navbar-icon" />
+        <h3 className="navbar-title">Foro</h3>
       </Link>
-      <Link to="/estadisticas" className="navbar-item">
-        <FaChartBar className="navbar-icon" />
-        <span className="navbar-title">Estadísticas</span>
-      </Link>
-      <Link to="/predicciones" className="navbar-item">
-        <FaChartBar className="navbar-icon" />
-        <span className="navbar-title">Predicciones</span>
-      </Link>
-      <Link to="/minijuegos" className="navbar-item">
-        <FaGamepad className="navbar-icon" />
-        <span className="navbar-title">Minijuegos</span>
-      </Link>
-      <div className="navbar-item notifications">
-        <FaBell className="navbar-icon" />
-        <span className="navbar-title">Notificaciones</span>
-        {notificaciones.length > 0 ? (
-          <ul className="notification-list">
-            {notificaciones.map((notificacion, index) => (
-              <li key={index}>{notificacion}</li>
-            ))}
-          </ul>
-        ) : (
-          <p className="notification-text">No tienes notificaciones nuevas</p>
+
+      {/* Nueva sección Estadísticas */}
+      <div className="navbar-item dropdown" onClick={toggleEstadisticas}>
+        <img src={FaChartBar} alt="Icon Estadísticas" className="navbar-icon" />
+        <h3 className="navbar-title">Estadísticas</h3>
+        {estadisticasOpen && (
+          <div className="dropdown-content">
+            <Link to="/partidos" className="dropdown-item">Partidos Pasados</Link>
+            <Link to="/partidos-directo" className="dropdown-item">Partidos en Directo</Link>
+            <Link to="/proximos-partidos" className="dropdown-item">Próximos Partidos</Link>
+          </div>
         )}
       </div>
+
+      {/* Sección Predicciones */}
+      <Link to="/predicciones" className="navbar-item">
+        <img src={FaChartBar} alt="Icon Foro" className="navbar-icon" />
+        <h3 className="navbar-title">Predicciones</h3>
+      </Link>
+
+      {/* Sección Minijuegos */}
+      <div className="navbar-item dropdown" onClick={toggleMinigames}>
+        <img src={FaGamepad} alt="Icon Minijuegos" className="navbar-icon" />
+        <h3 className="navbar-title">Minijuegos</h3>
+        {minigamesOpen && (
+          <div className="dropdown-content">
+            <Link to="/minijuegos/guess-the-player" className="dropdown-item">Guess the Player</Link>
+            <Link to="/minijuegos/tiro-libre" className="dropdown-item">Tiro Libre</Link>
+            <Link to="/minijuegos/bingo" className="dropdown-item">Bingo</Link>
+            <Link to="/minijuegos/wordle-diario" className="dropdown-item">Wordle Diario</Link>
+          </div>
+        )}
+      </div>
+
+      {/* Sección Notificaciones */}
+      <Link to="/notificaciones" className="navbar-item">
+        <img src={FaBell} alt="Icon Notificaciones" className="navbar-icon" />
+        <h3 className="navbar-title">Notificaciones</h3>
+      </Link>
+      
     </div>
   );
 }
