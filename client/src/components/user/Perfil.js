164026385ce182ec9ff68de7434f2081c7f44320
raquelcerezocarrion <raquel.cerezocarrion>
2024-12-06 20:34:03 +0100

    Actualizo form del perfil

--- client/src/components/user/Perfil.js
+++ client/src/components/user/Perfil.js
@@ -1,52 +1,253 @@
+import React, { useState, useEffect, useContext } from 'react';
+import { useNavigate, Link } from 'react-router-dom';
+import { FavoritosContext } from '../../context/FavoritosContext';
+import LoadInitialFavorites from './LoadInitialFavoritos';
+import UpdateFavoritesOnChange from './UpdateFavoritosOnChange';
+import { FaTimes } from 'react-icons/fa'; // Icono para la "X"
+import '../../styles/user/Perfil.css';
+
 function Perfil() {
+  const navigate = useNavigate();
+  const {
+    equiposFavoritos,
+    competicionesFavoritas,
+    removeEquipoFavorito,
+    removeCompeticionFavorita,
+  } = useContext(FavoritosContext);
+  const [userData, setUserData] = useState(null);
+  const [loading, setLoading] = useState(true);
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [editedData, setEditedData] = useState({
+    firstName: '',
+    lastName: '',
+    fotoPerfil: null,
+    equipoFavorito: [],
+    competicionesFavoritas: [],
+  });
+  const [showHelpForm, setShowHelpForm] = useState(false);
+  const [helpSubject, setHelpSubject] = useState('');
+  const [helpMessage, setHelpMessage] = useState('');
 
-  const handleLogin = () => {
-    if (!email || !password) {
-      alert('Por favor, completa todos los campos.');
-      return;
+  useEffect(() => {
+    const checkAuth = async () => {
+      try {
+        const response = await fetch('/api/auth/check-session', {
+          method: 'GET',
+          credentials: 'include',
+        });
+        const data = await response.json();
+        if (!data.isAuthenticated) {
+          navigate('/login');
+          return;
+        }
+        const userResponse = await fetch(`/api/users/${data.username}`, {
+          method: 'GET',
+          credentials: 'include',
+        });
+        if (!userResponse.ok) throw new Error('Error al obtener datos del usuario');
+
+        const userData = await userResponse.json();
+        setUserData(userData);
+        setEditedData({
+          firstName: userData.firstName,
+          lastName: userData.lastName,
+          fotoPerfil: userData.fotoPerfil,
+          equipoFavorito: userData.equipoFavorito || [],
+          competicionesFavoritas: userData.competicionesFavoritas || [],
+        });
+      } catch (error) {
+        console.error('Error:', error);
+        navigate('/login');
+      } finally {
+        setLoading(false);
+      }
+    };
+    checkAuth();
+  }, [navigate]);
+
+  const handleLogout = async () => {
+    try {
+      const response = await fetch('/api/users/logout', { method: 'POST' });
+      if (response.ok) navigate('/login');
+    } catch (error) {
+      console.error('Error al cerrar sesión:', error);
     }
-    alert(`Iniciaste sesión como ${email}`);
   };
 
-  return (
-    <div className="main-container">
-      {/* Anuncio izquierdo */}
-      <div className="sidebar">
-        <p>Espacio para anuncio</p>
-      </div>
+  const handleFotoChange = async (e) => {
+    const file = e.target.files[0];
+    if (file && userData) {
+      const formData = new FormData();
+      formData.append('fotoPerfil', file);
+      formData.append('username', userData.username);
+      try {
+        const response = await fetch('/api/users/uploads', { method: 'POST', body: formData });
+        if (!response.ok) throw new Error('Error al subir la imagen');
+        const data = await response.json();
+        setUserData((prevUserData) => ({
+          ...prevUserData,
+          fotoPerfil: data.imageUrl,
+        }));
+        alert('Imagen subida correctamente');
+      } catch (error) {
+        console.error('Error al subir la imagen:', error);
+      }
+    }
+  };
 
-      {/* Contenedor del formulario */}
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setEditedData({ ...editedData, [name]: value });
+  };
+
+  const handleSaveChanges = async () => {
+    try {
+      const updatedData = {
+        firstName: editedData.firstName,
+        lastName: editedData.lastName,
+        equipoFavorito: equiposFavoritos,
+        competicionesFavoritas: competicionesFavoritas,
+      };
+
+      const response = await fetch(`/api/users/${userData.username}`, {
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(updatedData),
+      });
+
+      if (!response.ok) throw new Error('Error al actualizar los datos del usuario');
+
+      setUserData(updatedData);
+      alert('Datos actualizados correctamente');
+    } catch (error) {
+      console.error('Error al actualizar los datos del usuario:', error);
+    }
+  };
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch('/api/auth/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email, password }),
+      });
+      if (response.ok) {
+        navigate('/dashboard');
+      } else {
+        alert('Credenciales inválidas.');
+      }
+    } catch (error) {
+      console.error('Error al iniciar sesión:', error);
+      alert('Error al iniciar sesión.');
+    }
+  };
+
+  if (loading) return <div>Cargando...</div>;
+  if (!userData) {
+    // Si no hay datos del usuario, muestra el formulario de inicio de sesión
+    return (
       <div className="perfil-container">
-        <p className="social-text">Inicia sesión con tu cuenta social</p>
-
-        <div className="social-login">
-          <button className="google-btn">Continuar con Google</button>
-        </div>
-
-        <form className="login-form" onSubmit={(e) => e.preventDefault()}>
+        <h1>LALIGA</h1>
+        <form onSubmit={handleLogin}>
+          <div className="social-login">
+            <button type="button" className="google">Continuar con Google</button>
+          </div>
+          <div className="divider">
+            <span>o</span>
+          </div>
+          <label htmlFor="email">Correo Electrónico</label>
           <input
             type="email"
-            placeholder="Correo Electrónico"
+            id="email"
+            placeholder="Ingresa tu correo electrónico"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
+            required
           />
+          <label htmlFor="password">Contraseña</label>
           <input
             type="password"
-            placeholder="Contraseña"
+            id="password"
+            placeholder="Ingresa tu contraseña"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
+            required
           />
-          <button className="login-btn" onClick={handleLogin}>
-            INICIAR SESIÓN
-          </button>
+          <button type="submit">INICIAR SESIÓN</button>
+          <a href="#">¿Olvidaste tu contraseña?</a>
         </form>
       </div>
+    );
+  }
 
-      {/* Anuncio derecho */}
-      <div className="sidebar">
-        <p>Espacio para anuncio</p>
+  return (
+    <div className="perfil-container">
+      <LoadInitialFavorites userData={userData} />
+      <UpdateFavoritesOnChange setEditedData={setEditedData} />
+
+      <h1>Perfil de {userData.username}</h1>
+      <div className="perfil-content">
+        <div className="perfil-left">
+          <div className="perfil-photo">
+            {editedData.fotoPerfil ? (
+              <img src={editedData.fotoPerfil} alt="Foto de perfil" className="profile-image" />
+            ) : (
+              <div className="placeholder-image">No profile photo</div>
+            )}
+            <input type="file" accept="image/*" onChange={handleFotoChange} />
+          </div>
+          <div className="perfil-info">
+            <label>Nombre:
+              <input type="text" name="firstName" value={editedData.firstName} onChange={handleInputChange} />
+            </label>
+            <label>Apellido:
+              <input type="text" name="lastName" value={editedData.lastName} onChange={handleInputChange} />
+            </label>
+          </div>
+        </div>
+        <div className="perfil-right">
+          <div className="favoritos">
+            <h3>Equipos Favoritos</h3>
+            <ul>
+              {equiposFavoritos.map((equipo, index) => (
+                <li key={`${equipo}-${index}`} className="favorito-item">
+                  {equipo}
+                  <button className="remove-btn" onClick={() => removeEquipoFavorito(equipo)}>
+                    <FaTimes color="red" />
+                  </button>
+                </li>
+              ))}
+            </ul>
+            <Link to="/perfil/anadir-equipo-favorito">
+              <button>Añadir equipo favorito</button>
+            </Link>
+
+            <h3>Competiciones Favoritas</h3>
+            <ul>
+              {competicionesFavoritas.map((competicion, index) => (
+                <li key={`${competicion}-${index}`} className="favorito-item">
+                  {competicion}
+                  <button className="remove-btn" onClick={() => removeCompeticionFavorita(competicion)}>
+                    <FaTimes color="red" />
+                  </button>
+                </li>
+              ))}
+            </ul>
+            <Link to="/perfil/anadir-competicion-favorita">
+              <button>Añadir competición favorita</button>
+            </Link>
+          </div>
+        </div>
+      </div>
+
+      <div className="perfil-actions">
+        <button onClick={handleSaveChanges}>Guardar Cambios</button>
+        <button onClick={handleLogout}>Cerrar Sesión</button>
+        <button onClick={() => setShowHelpForm(true)}>Ayuda</button>
       </div>
     </div>
   );
