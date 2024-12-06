9442cebf7c8a2ebc3d664663dc41a9b0d766d908
lorensation <lsanztrucharte@gmail.com>
2024-12-06 11:10:07 +0100

    debugging middleware auth session

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -1,4 +1,5 @@
 // middleware/authMiddleware.js
+console.log("Sesión actual:", req.session);
 module.exports = (req, res, next) => {
     if (req.session && req.session.userId) {
         next(); // El usuario está autenticado, continuar con la solicitud
