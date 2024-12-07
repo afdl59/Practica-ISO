bf65465557337ae9aaf52f776f81c9ea188e2e80
Ignacio Arvilla <arvillaignacio@gmail.com>
2024-12-07 17:39:39 +0100

    probando endpoint de ranking de puntos

--- client/src/components/stats/Rankings.js
+++ client/src/components/stats/Rankings.js
@@ -1,9 +1,26 @@
 import React, { useEffect, useState } from 'react';
 
 function Rankings() {
+    const [leaderboard, setLeaderboard] = useState([]);
+
+    useEffect(() => {
+        const fetchLeaderboard = async () => {
+            const response = await fetch('/api/users/predictionsRanking');
+            if (!response.ok) {
+                console.error('Error al obtener el ranking');
+                return;
+            }
+            const leaderboardData = await response.json();
+            setLeaderboard(leaderboardData);
+        };
+
+        fetchLeaderboard();
+    }, []);
+
     return (
-        <div>
-            <h1>Rankings</h1>
+        <div className="leaderboard-container">
+            <h1>Ranking de Minijuegos</h1>
+            <pre>{JSON.stringify(leaderboard, null, 2)}</pre>
         </div>
     );
 }
