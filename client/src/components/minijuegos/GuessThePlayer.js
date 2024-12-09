041d0f0ab6ce789360c737a3ca6f7a249e677048
lorensation <lsanztrucharte@gmail.com>
2024-12-09 19:35:55 +0100

    importando leaderboard

--- client/src/components/minijuegos/GuessThePlayer.js
+++ client/src/components/minijuegos/GuessThePlayer.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { useLeaderboard } from '../../context/LeaderboardContext';
 import '../../styles/minijuegos/GuessThePlayer.css';
 
 import Messi from '../../assets/players/Messi.jpg';
