5b671625b00956272f60eada0edfad11c11daff5
lorensation <lsanztrucharte@gmail.com>
2024-12-09 19:07:27 +0100

    probando error de busqueda en mongo desde el endpoint

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -484,13 +484,13 @@ exports.updatePremiumStatusByEmail = async (email) => {
     }
   };
 
-  exports.getAllUsers = async (req, res) => {
+exports.getAllUsers = async (req, res) => {
     try {
-      const users = await User.find({}, 'username');
-      res.status(200).json(users);
+        const users = await User.find({}, { username: 1 });
+        res.status(200).json(users);
     } catch (error) {
-      console.error('Error al obtener usuarios:', error);
-      res.status(500).json({ message: 'Error al obtener usuarios' });
+        console.error('Error al obtener usuarios:', error);
+        res.status(500).json({ message: 'Error al obtener usuarios' });
     }
-  };
-  
+};
+
